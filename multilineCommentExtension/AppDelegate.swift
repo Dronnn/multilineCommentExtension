0189f8dee1e7ddb71f3fb5330ba951bcf5070396
Andrew Vanyurin <dddron@gmail.com>
2017-10-13 11:41:16 +0300

    Initial Commit

---
+++ multilineCommentExtension/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  multilineCommentExtension
+//
+//  Created by Andrew Vanyurin on 13/10/2017.
+//  Copyright © 2017 Andrew Vanyurin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ multilineCommentExtension/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  multilineCommentExtension
+//
+//  Created by Andrew Vanyurin on 13/10/2017.
+//  Copyright © 2017 Andrew Vanyurin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
